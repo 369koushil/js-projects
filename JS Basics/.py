269e74b0425b37337dab54299c32ac25c0740c4b
koushil <154089176+369koushil@users.noreply.github.com>
2024-11-16 01:04:26 +0530

    Delete JS Basics directory

--- JS Basics/.py
+++
@@ -1,5 +0,0 @@
-arr=[1,2,3,4]
-
-from functools import reduce
-e=lambda i,j:i+j
-print(reduce(e,arr))
--- JS Basics/strings.js
+++
@@ -1,7 +0,0 @@
-
-// let uname=prompt("enter your name");
-// let uname2=uname.trim();
-// let uname3=uname2.replaceAll(" ","");
-// console.log(`@${uname3}${uname3.length}`);
-
-
